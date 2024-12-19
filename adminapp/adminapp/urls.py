80ddc37e4ec33ded55a52a2b31c88000dd92ac8d
PyProfileCoder <161020715+PyProfileCoder@users.noreply.github.com>
2024-12-19 22:29:23 +0530

    Update urls.py

--- adminapp/adminapp/urls.py
+++ adminapp/adminapp/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from polls import views
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
 ]
