625bac631a8e06c7cac4544fd00ee0a75537b2fe
PyProfileCoder <161020715+PyProfileCoder@users.noreply.github.com>
2024-12-19 16:44:39 +0000

    added polls app to adminapp

--- adminapp/adminapp/settings.py
+++ adminapp/adminapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "finance"
+    "finance",
+    "polls"
 ]
 
 MIDDLEWARE = [
